2287d421c0212115d855e32a1ff119c58b3e7afc
Janko <janko.prester@gmail.com>
2021-10-12 21:01:59 +0200

    create folder on runtime fix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const json2html = require("node-json2html");
 const fs = require("fs-extra");
 
 const fileName = process.argv.slice(2)[0];
+const dir = "./output";
 
 const style =
   '@import url(https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600&display=swap);@import url(https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,300;1,700;1,900&display=swap);body{background-color:#f2f2f2;font-family:-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Fira Sans","Droid Sans","Helvetica Neue",sans-serif;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;font-size:16px}body,div,h1,h2,h3,h4,h5,h6,li,ol,p,table,td,th,tr,ul{margin:0;padding:0;box-sizing:border-box}.content{margin:0 auto;width:960px}.content h1{font-size:24px}.model-config ul{list-style:none;padding:0;display:flex}.model-config ul li{margin-right:12px}.output-table{border-collapse:collapse;margin-top:20px;text-align:center}.output-table td,th{padding:4px}.output-table td{border:1px solid #ccc}';
@@ -108,6 +109,10 @@ if (fileName) {
   let rawdata = fs.readFileSync(fileName);
   let parsedData = JSON.parse(rawdata);
 
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+
   writeHtmlFromScoresJson(
     parsedData,
     `output/${fileName.replace(".json", "")}_output.html`
@@ -115,7 +120,7 @@ if (fileName) {
 }
 
 function writeHtmlFromScoresJson(jsonFile, htmlTableFile) {
-  let data = jsonFile[0];
+  let data = jsonFile;
 
   const formattedData = Object.keys(data.versions).map((item) => {
     return { ...data.results[item], name: item, version: data.versions[item] };
